7ecf673237a166de81dc1327f1f4b46a5727361e
DeathInTheAir <41319697+DeathInTheAir@users.noreply.github.com>
2026-08-22 17:21:03 +0800

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const NEZHA_SERVER = process.env.NEZHA_SERVER || '';
 const NEZHA_KEY = process.env.NEZHA_KEY || '';             
 const DOMAIN = process.env.DOMAIN || 'your-domain.com';    
 const AUTO_ACCESS = process.env.AUTO_ACCESS || false;      
-const SUB_PATH = process.env.SUB_PATH || 'vercel';           
+const SUB_PATH = process.env.SUB_PATH || 'we5abV';           
 const NAME = process.env.NAME || 'Vercel';                       
 const PORT = process.env.PORT || 3000;                    
 
